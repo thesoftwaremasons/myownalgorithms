32bf2baa2823fc45d9b03a4f77995a9046481c8e
Babafemi Ogundipe <smiles2k6@gmail.com>
2022-03-25 21:28:26 +0100

    added get

---
+++ arrays/get/get.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+struct Array
+{
+    int A[10];
+    int length;
+    int size;
+};
+
+int get(Array *arr, int value)
+{
+    int i;
+    for (i = 0; i < arr->length; i++)
+    {
+        if (arr->A[i] == value)
+            return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    Array arr1 = {{2, 3, 9, 16, 18}, 5, 10};
+    int result = get(&arr1, 12);
+    printf("%d ", result);
+    /* code */
+    return 0;
+}
