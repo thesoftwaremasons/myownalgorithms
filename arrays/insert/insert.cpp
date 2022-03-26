7aaaa6a131da272a925a4bf2cf042822d035af9b
Babafemi Ogundipe <smiles2k6@gmail.com>
2022-03-26 12:37:47 +0100

    made modification to set and insert

--- arrays/insert/insert.cpp
+++ arrays/insert/insert.cpp
@@ -6,11 +6,15 @@ struct Array
     int size;
 };
 
-void set(Array *arr, int index, int value)
+void insert(Array *arr, int index, int value)
 {
- 
+    int i;
+    for (i = arr->length; i > index; i--)
+    {
+        arr->A[i] = arr->A[i - 1];
+    }
     arr->A[index] = value;
-    
+    arr->length = arr->length + 1;
 }
 void display(Array arr)
 {
@@ -26,7 +30,7 @@ int main(int argc, char const *argv[])
     printf("\n Array elements are\n");
     display(arr1);
     printf("\n Set Array elements are\n");
-    set(&arr1, 2, 8);
+    insert(&arr1, 2, 8);
     display(arr1);
 
     return 0;
